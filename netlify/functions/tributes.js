a37302ecd04d70db9f635b63a8a81ec2ce721dc4
Skdaktari001 <tech.daktari@gmail.com>
2026-02-07 15:26:39 +0300

    making sure all work

--- netlify/functions/tributes.js
+++ netlify/functions/tributes.js
@@ -1,21 +1,37 @@
 export async function handler(event) {
-    const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwHVNKPTGBjLXPYJOEv_yItH6X66KbKB75TnCpBRyRdfveX4Oth0Jj_uVcUlLwpi2LS9A/exec';
+    try {
+      const SCRIPT_URL =
+        'https://script.google.com/macros/s/AKfycbypBBxnMMl5uJe7C6JQFEJXw6Qy26XSMDZ4dqZfVuQr2i7QNQ5cRSqJXSZoXFN9ETu73w/exec';
   
-    const res = await fetch(SCRIPT_URL, {
-      method: event.httpMethod,
-      headers: { 'Content-Type': 'application/json' },
-      body: event.body
-    });
+      const res = await fetch(SCRIPT_URL, {
+        method: event.httpMethod,
+        headers: { 'Content-Type': 'application/json' },
+        body: event.httpMethod === 'GET' ? null : event.body
+      });
   
-    const text = await res.text();
+      const text = await res.text();
   
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
-      },
-      body: text
-    };
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json'
+        },
+        body: text
+      };
+  
+    } catch (err) {
+      return {
+        statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          status: 'error',
+          message: err.message || String(err)
+        })
+      };
+    }
   }
   
